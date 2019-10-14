2e5ce3bf75f2aac5f6118d15d217f47dd6041d0b
dengyuli0904 <912617726@qq.com>
2019-10-14 14:10:18 +0800

    dealer settings save

---
+++ source/application/common/model/DistributorsSetting.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lily
+ * Date: 19/10/12
+ * Time: 下午7:42
+ */
+
+namespace app\common\model;
+
+
+class DistributorsSetting
+{
+
+}
---
+++ source/application/store/model/DistributorsSetting.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lily
+ * Date: 19/10/12
+ * Time: 下午7:51
+ */
+
+namespace app\store\model;
+
+
+class DistributorsSetting
+{
+
+}
