0a04ca78ecf4597aa99102cd7bd6803c1b6074c2
lilyDeng1 <lily.d@lifebyte.com.au>
2019-11-08 18:41:51 +0800

    添加coupon model

---
+++ source/application/store/model/Coupon.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: L
+ * Date: 2019/11/8
+ * Time: 16:15
+ */
+
+namespace app\store\model;
+use app\common\model\Coupon as CouponModel;
+
+
+class Coupon extends CouponModel
+{
+
+}
